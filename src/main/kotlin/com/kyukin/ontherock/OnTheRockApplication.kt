37cdd55ca4e2ceadff5e3a815515105263911d64
leekyukin <leekujin14@gmail.com>
2022-10-25 09:18:35 +0900

    SET :: initial

---
+++ src/main/kotlin/com/kyukin/ontherock/OnTheRockApplication.kt
@@ -0,0 +1,11 @@
+package com.kyukin.ontherock
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OnTheRockApplication
+
+fun main(args: Array<String>) {
+    runApplication<OnTheRockApplication>(*args)
+}
---
+++ src/test/kotlin/com/kyukin/ontherock/OnTheRockApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kyukin.ontherock
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OnTheRockApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
